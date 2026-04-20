76780aa7b1cda3fad9eca89e7b677876ddececb1
전재민 <beforejamn1@gmail.com>
2026-04-20 17:07:35 +0900

    FEAT:SWEA_14510_나무높이-PASS

---
+++ D0420/SWEA_14510_나무높이.java
@@ -0,0 +1,55 @@
+package D0420;
+
+import java.io.*;
+import java.util.*;
+
+public class SWEA_14510_나무높이 {
+    static final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static final BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    static final StringBuilder sb = new StringBuilder();
+
+    static StringTokenizer st;
+
+    public static void main(String[] args) throws IOException {
+        int T = Integer.parseInt(br.readLine());
+        for (int t = 1; t <= T; t++) {
+            sb.append("#").append(t).append(" ");
+            int N = Integer.parseInt(br.readLine());
+            int[] trees = new int[N];
+
+            int maxlen = 0;
+            st = new StringTokenizer(br.readLine());
+            for (int i = 0; i < N; i++) {
+                trees[i] = Integer.parseInt(st.nextToken());
+                maxlen = maxlen > trees[i] ? maxlen : trees[i];
+            }
+
+            Arrays.sort(trees);
+            int oddDays = 0;
+            int evenDays = 0;
+            for (int i = 0; i < N; i++) {
+                int needDay = maxlen - trees[i];
+                oddDays += needDay % 2;
+                evenDays += needDay / 2;
+            }
+
+            while (evenDays > oddDays + 1) {
+                evenDays--;
+                oddDays += 2;
+            }
+
+            int result = 0;
+            if (evenDays >= oddDays) {
+                result = evenDays * 2;
+            } else if (oddDays > evenDays) {
+                result = oddDays * 2 - 1;
+            }
+
+            sb.append(result).append("\n");
+        }
+        bw.write(sb.toString());
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
